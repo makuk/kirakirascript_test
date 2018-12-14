7b4f9825105cc94a084917509e5aafd8a0aba37e
makuk <health092154@gmail.com>
2018-12-14 17:56:06 +0900

    Add files via upload

--- lexer.js
+++ lexer.js
@@ -22,8 +22,14 @@ for(let i=0;i<myCode.length;i++) {
         case '\r':
             break; //공백
 
-        case '\"': 
-            state = 'string'; //문자열
+        case '\"':
+            str = c;
+            state = 'string1'; //문자열
+            break;
+
+        case '\'':
+            str = c;
+            state = 'string2';
             break;
 
         case '0':
@@ -36,96 +42,287 @@ for(let i=0;i<myCode.length;i++) {
         case '7':
         case '8':
         case '9':
+            str = c;
             state = 'number'; //숫자
             break;
 
         case '+':
-            if(check(i+1) === '+') {
-                tokenArray.push(new Token('OP', '++', line)); //증가 연산자
+            if(checkNext(i, '+')) {
+                tokPush('OP', '++', line); //증가 연산자
                 i++;
             }
-            else if(check(i+1) === '=') {
-                tokenArray.push(new Token('OP', '+=', line)); //증가후 대입
+            else if(checkNext(i, '=')) {
+                tokPush('OP', '+=', line); //증가후 대입
                 i++;
             }
-            else if(check(i+1) === '>') {
-                tokenArray.push(new Token('OP', '+>', line)); //그냥 만들었는데 용도는 나중에 생각할거임
+            else if(checkNext(i, '>')) {
+                tokPush('OP', '+>', line); //함수 인자 추가
                 i++;
             }
             else {
-                tokenArray.push(new Token('OP', '+', line)); //더하기
+                tokPush('OP', '+', line); //더하기
             }
             break;
 
         case '-':
-            if(check(i+1) === '-') {
-                tokenArray.push(new Token('OP', '--', line)); //감소 연산자
+            if(checkNext(i, '-')) {
+                tokPush('OP', '--', line); //감소 연산자
                 i++;
             }
-            else if(check(i+1) === '=') {
-                tokenArray.push(new Token('OP', '-=', line)); //감소후 대입
+            else if(checkNext(i, '=')) {
+                tokPush('OP', '-=', line); //감소후 대입
                 i++;
             }
-            else if(check(i+1) === '>') {
-                tokenArray.push(new Token('OP', '->', line)); //함수 인자 받기
+            else if(checkNext(i, '>')) {
+                tokPush('OP', '->', line); //함수 인자 받기
                 i++;
             }
             else {
-                tokenArray.push(new Token('OP', '-', line)); //빼기
+                tokPush('OP', '-', line); //빼기
             }
             break;
 
         case '*':
-            if(check(i+1) === '*') {
-                tokenArray.push(new Token('OP', '**', line)); //거듭제곱
+            if(checkNext(i, '*')) {
+                tokPush('OP', '**', line); //거듭제곱
                 i++;
             }
-            else if(check(i+1) === '=') {
-                tokenArray.push(new Token('OP', '*=', line)); //곱한후 대입
+            else if(checkNext(i, '=')) {
+                tokPush('OP', '*=', line); //곱한후 대입
                 i++;
             }
             else {
-                tokenArray.push(new Token('OP', '*', line)); //곱하기
+                tokPush('OP', '*', line); //곱하기
             }
             break;
 
         case '/':
-            if(check(i+1) === '/') {
+            if(checkNext(i, '/')) {
                 state = 'com1'; //한줄주석
             }
-            else if(check(i+1) === '*') {
+            else if(checkNext(i, '*')) {
                 state = 'com2'; //여러줄주석
             }
-            else if(check(i+1) === '%') {
-                tokenArray.push(new Token('OP', '/%', line)); //몫 구하기
+            else if(checkNext(i, '%')) {
+                tokPush('OP', '/%', line); //몫 구하기
                 i++;
             }
-            else if(check(i+1) === '=') {
-                tokenArray.push(new Token('OP', '/=', line)); //나눈후 대입
+            else if(checkNext(i, '=')) {
+                tokPush('OP', '/=', line); //나눈후 대입
                 i++;
             }
             else {
-                tokenArray.push(new Token('OP', '/', line)); //나누기
+                tokPush('OP', '/', line); //나누기
             }
             break;
 
         case '%':
-            if(check(i+1) === '=') {
-                tokenArray.push(new Token('OP', '%=', line)); //나머지 구한후 대입
+            if(checkNext(i, '=')) {
+                tokPush('OP', '%=', line); //나머지 구한후 대입
                 i++;
             }
             else {
-                tokenArray.push(new Token('OP', '%', line)); //나머지 구하기
+                tokPush('OP', '%', line); //나머지 구하기
             }
             break;
         
+        case '=':
+            if(checkNext(i, '=')) {
+                tokPush('OP', '==', line); //비교
+                i++;
+            }
+            else {
+                tokPush('OP', '=', line); //대입
+            }
+            break;
+        
+        case '<':
+            if(checkNext(i, '<')) {
+                tokPush('OP', '<<', line); //비트쉬프트
+                i++;
+            }
+            else if(checkNext(i, '-')) {
+                tokPush('OP', '<-', line); //함수 인자
+                i++;
+            }
+            else if(checkNext(i, '=')) {
+                tokPush('OP', '<=', line); //작거나 같음
+                i++;
+            }
+            else {
+                tokPush('OP', '<', line); //작음
+            }
+            break;
+        
+        case '>':
+            if(checkNext(i, '>')) {
+                tokPush('OP', '>>', line); //비트쉬프트
+                i++;
+            }
+            else if(checkNext(i, '=')) {
+                tokPush('OP', '>=', line); //크거나 같음
+                i++;
+            }
+            else {
+                tokPush('OP', '>', line); //큼
+            }
+            break;
+        
+        case '!':
+            if(checkNext(i, '=')) {
+                tokPush('OP', '!=', line); //같지 않음
+                i++;
+            }
+            else {
+                tokPush('OP', '!', line); //부정
+            }
+            break;
 
+        case '?':
+            tokPush('OP', '?', line);
+            break;
+        
+        case ':':
+            tokPush('OP', ':', line);
+            break;
+        
+        case '&':
+            if(checkNext(i, '&')) {
+                tokPush('OP', '&&', line);
+                i++;
+            }
+            else if(checkNext(i, '=')) {
+                tokPush('OP', '&=', line);
+                i++;
+            }
+            else {
+                tokPush('OP', '&', line);
+            }
+            break;
+
+        case '|':
+            if(checkNext(i, '|')) {
+                tokPush('OP', '||', line);
+                i++;
+            }
+            else if(checkNext(i, '=')) {
+                tokPush('OP', '|=', line);
+                i++;
+            }
+            else {
+                tokPush('OP', '|', line);
+            }
+            break;
+
+        case '^':
+            if(checkNext(i, '^')) {
+                tokPush('OP', '^^', line);
+                i++;
+            }
+            else if(checkNext(i, '=')) {
+                tokPush('OP', '^=', line);
+                i++;
+            }
+            else {
+                tokPush('OP', '^', line);
+            }
+            break;
+
+        case '@':
+            tokPush('OP', '@', line);
+            break;
+        
+        case '#':
+            tokPush('OP', '#', line);
+            break;
+        
+        case '$':
+            tokPush('OP', '$', line);
+            break;
+
+        case '~':
+            tokPush('OP', '~', line);
+            break;
+
+        case ':':
+            tokPush('OP', ':', line);
+            break;
+        
+        case ';':
+            tokPush('OP', ';', line);
+            break;
+
+        case ',':
+            tokPush('OP', ',', line);
+            break;
+
+        case '.':
+            tokPush('OP', ',', line);
+            break;
+
+        case '(':
+            tokPush('BRK', '(', line);
+            break;
+        
+        case ')':
+            tokPush('BRK', ')', line);
+            break;
+
+        case '{':
+            tokPush('BRK', '{', line);
+            break;
+
+        case '}':
+            tokPush('BRK', '}', line);
+            break;
+
+        case '[':
+            tokPush('BRK', '[', line);
+            break;
+
+        case ']':
+            tokPush('BRK', ']', line);
+            break;
+
+        default:
+            state = 'identifier';
         }
+        break;
+    
+    case 'number':
+        if(needCut.includes(c)) {
+            tokPush('number', str, line);
+            str = '';
+            i--;
+            state = 'none';
+        }
+        else {
+            str += c;
+        }
+        break;
+
+    case 'string1':
+        break;
+
+    case 'com1':
+        if(c === '\n') {
+            line++;
+            state = 'none';
+        }
+
+    case 'com2':
+
+    case 'identifier':
+
     }
 }
 
-function check(pos) {
-    return myCode[pos];
+function checkNext(pos, c) {
+    return myCode[pos+1] === c;
+}
+
+function tokPush(type, value, line) {
+    tokenArray.push((type, value, line));
 }
 
 module.exports = tokenArray;
